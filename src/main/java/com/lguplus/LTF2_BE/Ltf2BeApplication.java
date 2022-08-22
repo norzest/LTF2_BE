f9d7a5d05552d5a696e923c4dae843cbff15af9d
khyunchoi <khyunchoi@naver.com>
2022-08-22 11:07:21 +0900

    [LT2-22] init project

---
+++ src/main/java/com/lguplus/LTF2_BE/Ltf2BeApplication.java
@@ -0,0 +1,13 @@
+package com.lguplus.LTF2_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ltf2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ltf2BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lguplus/LTF2_BE/Ltf2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lguplus.LTF2_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ltf2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
