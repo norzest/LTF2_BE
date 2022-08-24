ca141e61392aa8f1e1fd0b8b77cd26b8c5aaed4f
kwonmoto <spadekwon@gmail.com>
2022-08-24 14:23:05 +0900

    [LT2-16] Search Controller

--- src/main/java/com/lguplus/LTF2_BE/api/util/ManuFacturingCompanyConverter.java
+++ src/main/java/com/lguplus/LTF2_BE/api/util/ManuFacturingCompanyConverter.java
@@ -1,6 +1,6 @@
 package com.lguplus.LTF2_BE.api.util;
 
-import com.lguplus.LTF2_BE.core.domain.ManuFacturingCompany;
+import com.lguplus.LTF2_BE.core.domain.ManufacturingCompany;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
@@ -8,18 +8,18 @@ import java.util.EnumSet;
 import java.util.NoSuchElementException;
 
 @Converter
-public class ManuFacturingCompanyConverter implements AttributeConverter<ManuFacturingCompany, Integer> {
+public class ManuFacturingCompanyConverter implements AttributeConverter<ManufacturingCompany, Integer> {
 
     // int 값으로 변환 후 DB에 저장
     @Override
-    public Integer convertToDatabaseColumn(ManuFacturingCompany attribute) {
+    public Integer convertToDatabaseColumn(ManufacturingCompany attribute) {
         return attribute.getCode();
     }
 
     // DB 값을 문자열로 변환해서 불러오기
     @Override
-    public ManuFacturingCompany convertToEntityAttribute(Integer dbData) {
-        return EnumSet.allOf(ManuFacturingCompany.class).stream()
+    public ManufacturingCompany convertToEntityAttribute(Integer dbData) {
+        return EnumSet.allOf(ManufacturingCompany.class).stream()
                 .filter(e -> e.getCode().equals(dbData))
                 .findAny()
                 .orElseThrow(NoSuchElementException::new);
--- src/main/java/com/lguplus/LTF2_BE/core/repository/PhoneRepository.java
+++ src/main/java/com/lguplus/LTF2_BE/core/repository/PhoneRepository.java
@@ -1,5 +1,6 @@
 package com.lguplus.LTF2_BE.core.repository;
 
+import com.lguplus.LTF2_BE.core.domain.ManufacturingCompany;
 import com.lguplus.LTF2_BE.core.domain.Phone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,7 @@ import java.util.List;
 public interface PhoneRepository extends JpaRepository<Phone, Long> {
 
     List<Phone> findAllByOrderById();
+
+    List<Phone> findByTitleNameContainsOrModelContainsOrManufacturingCompany(String titleName, String model, ManufacturingCompany company);
+
 }
