22e4fa153184bda6baf02863dd04833ab3e750d0
khyunchoi <khyunchoi@naver.com>
2022-09-01 17:39:52 +0900

    [LT2-36] 주간판매량 로직 추가

--- src/main/java/com/lguplus/LTF2_BE/core/repository/OrdersRepository.java
+++ src/main/java/com/lguplus/LTF2_BE/core/repository/OrdersRepository.java
@@ -4,5 +4,11 @@ import com.lguplus.LTF2_BE.core.domain.Orders;
 import com.lguplus.LTF2_BE.core.domain.Phone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
+
+    // DB에서 phoneId를 기준으로 startDate와 endDate 사이에 주문된 데이터를 조회한 후 반환
+    List<Orders> findByPhoneIdAndOrderDateBetween(Long phoneId, LocalDateTime startDate, LocalDateTime endDate);
 }
