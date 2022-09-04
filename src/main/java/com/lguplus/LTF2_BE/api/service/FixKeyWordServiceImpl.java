e97f599fc024be33b251f9936825ee388d6e7d0f
영효 심 <dark1203@naver.com>
2022-09-04 18:54:55 +0900

    update Search

--- src/main/java/com/lguplus/LTF2_BE/api/service/FixKeyWordServiceImpl.java
+++ src/main/java/com/lguplus/LTF2_BE/api/service/FixKeyWordServiceImpl.java
@@ -1,5 +1,7 @@
 package com.lguplus.LTF2_BE.api.service;
 
+import com.lguplus.LTF2_BE.api.util.ChangeKeyWordUtil;
+import com.lguplus.LTF2_BE.core.repository.ChangeKeywordRepository;
 import com.lguplus.LTF2_BE.core.repository.KeywordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ import static com.lguplus.LTF2_BE.api.util.SymSpell.CreateDictionary;
 public class FixKeyWordServiceImpl implements FixKeyWordService{
 
     private final KeywordRepository keywordRepository;
+
+    private final ChangeKeywordRepository changeKeywordRepository;
 
     /**
      * 검색어의 편집거리를 계산하여 DB 에 있는 문자열 리스트 중
@@ -26,33 +30,15 @@ public class FixKeyWordServiceImpl implements FixKeyWordService{
 
         // 생성된 사전에서 keyWord 의 편집 거리를 계산하여
         // 편집거리가 낮은 가장 적절한 문자열로 반환한다.
+        // ex. 갤넉시 -> 갤럭시
         String fixed = Correct(keyWord, "");
 
         // 사용자가 자주 검색할만한 키워드들을
-        // DB 에 저장되어있는 같은 뜻의 값들로 변환
-        switch (fixed) {
-            case "아이폰":
-                fixed = "iPhone";
-                break;
-            case "samsung":
-                fixed = "삼성";
-                break;
-            case "apple":
-                fixed = "애플";
-                break;
-            case "플립":
-                fixed = "Flip";
-                break;
-            case "폴더":
-            case "폴더블":
-                fixed = "Fold";
-                break;
-            default:
-                break;
-        }
-
-        // 수정된 값을 반환
-        return fixed;
+        // DB 에 저장되어있는 같은 뜻의 값으로 변환한 후
+        // 변한된 값을 반환 
+        // ex. 상품의 제조사가 삼성 일 경우 samsung 으로 검색하면 결과가 나오지 않는다
+        // ex. 따라서 samsung 을 삼성으로 변환하여 반환
+        return ChangeKeyWordUtil.changeKeyWord(fixed, changeKeywordRepository.findAll());
     }
 
     // DB 에 저장되어있는 문자열들을 가져와 사전을 생성한다
---
+++ src/main/java/com/lguplus/LTF2_BE/api/util/ChangeKeyWordUtil.java
@@ -0,0 +1,22 @@
+package com.lguplus.LTF2_BE.api.util;
+
+import com.lguplus.LTF2_BE.core.domain.ChangeKeyword;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChangeKeyWordUtil {
+    private static Map<String, String> dict = null;
+
+    public static String changeKeyWord(String keyWord, List<ChangeKeyword> lst) {
+        if(dict == null) {
+            dict = new HashMap<>();
+
+            for(ChangeKeyword ck : lst)
+                dict.put(ck.getBeforeWord(), ck.getAfterWord());
+        }
+
+        return dict.getOrDefault(keyWord, keyWord);
+    }
+}
